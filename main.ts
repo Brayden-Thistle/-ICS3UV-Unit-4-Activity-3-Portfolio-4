33eb409afa6dfd832ee217dd96415984e988137c
Brayden Thistle <bthistle@huronperthcatholic.ca>
2026-01-04 23:46:18 +0000

    started program

---
+++ main.ts
@@ -0,0 +1,6 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2026-01-03
+ * @fileoverview this program prompts the user for 10 random numbers and then the program finds the largest value insdie it and displays it along with the list of numbers.
+ */
